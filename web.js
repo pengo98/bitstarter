1fe22b4638dd9f31892fddc395f1d143fd068fe2
Nelson Lin <nelson.lin@euroka.com.au>
2013-07-11 13:01:52 +0000

    test

--- web.js
+++ web.js
@@ -1,9 +1,15 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  
+    var data = fs.readFileSync('index.html');
+    var buf = new Buffer(data);
+
+    //response.send(buf.toString('ascii',0,25));
+    response.send('blahal');
 });
 
 var port = process.env.PORT || 5000;
